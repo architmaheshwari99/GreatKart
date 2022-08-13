c41a69440309a81f0a94cf0cfd95e176c8bc0788
maheshwari <archit.maheshwari@payufin.com>
2022-08-13 23:24:02 +0530

    making password read-only and updating AccountAdmin

--- accounts/models.py
+++ accounts/models.py
@@ -44,7 +44,7 @@ class Account(AbstractBaseUser):
     phone_number = models.CharField(max_length=50)
 
     #required
-    data_joined = models.DateTimeField(auto_now_add=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now_add=True)
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
