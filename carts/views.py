6b9e6ef26a76ae38dc4f7cb168b8d3f586011438
maheshwari <archit.maheshwari@payufin.com>
2022-08-14 17:41:03 +0530

    Add to cart functionality-backend

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from carts.models import Cart, CartItem
+from store.models import Product
+
+
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id = _cart_id(request)) # use session id as cart id
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(cart_id=_cart_id(request))
+
+    cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(product=product, cart=cart, quantity=1)
+        cart_item.save()
+    return redirect('cart')
+
 
 def cart(request):
     return render(request, 'store/cart.html')
