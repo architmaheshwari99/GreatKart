88d95362ccf37f3ea058281e55a314fa9b545bfe
maheshwari <archit.maheshwari@payufin.com>
2022-08-15 17:48:31 +0530

    Product Variation-I

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
@@ -13,6 +14,8 @@ def _cart_id(request):
     return cart
 
 def add_cart(request, product_id):
+    color = request.GET['color']
+    size = request.GET['size']
     product = Product.objects.get(id=product_id)
     try:
         cart = Cart.objects.get(cart_id = _cart_id(request)) # use session id as cart id
