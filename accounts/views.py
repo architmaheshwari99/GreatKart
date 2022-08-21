4aa16bbd91a353fd3537b7a2638ef1b39c1f7277
maheshwari <archit.maheshwari@payufin.com>
2022-08-21 23:15:24 +0530

    Alerts and messages in user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from accounts.forms import RegistrationForm
@@ -18,6 +19,8 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password )
             user.phone_number = phone_number
             user.save()
+            messages.success(request, 'Registration Successful')
+            return redirect('register')
     else:
         form = RegistrationForm()
     context = {
